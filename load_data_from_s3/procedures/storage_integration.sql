a98588b8235643949d149ffeaeefc009e698dd9f
Ivan Felipe Caro Lemus <ivan.caro@globant.com>
2024-06-13 14:31:19 -0500

    update create procedure

--- load_data_from_s3/procedures/storage_integration.sql
+++ load_data_from_s3/procedures/storage_integration.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE create_storage_integration_to_s3(sotrage_name VARCHAR, iam_role VARCHAR, bucket_name VARCHAR)
+CREATE PROCEDURE create_storage_integration_to_s3(storage_name VARCHAR, iam_role VARCHAR, bucket_name VARCHAR)
     RETURNS VARCHAR
     LANGUAGE SQL
 AS
@@ -6,12 +6,13 @@ AS
     DECLARE
         message_out VARCHAR DEFAULT 'Storage integration to S3 SUCCESFULLY CREATED';
     BEGIN
-        CREATE OR REPLACE STORAGE INTEGRATION :sotrage_name
+        QUERY := 'CREATE OR REPLACE STORAGE INTEGRATION '|| storage_name ||'
             type = external_stage
             storage_provider = s3
             enabled = true
-            storage_aws_role_arn = :iam_role
-            storage_allowed_locations = (:bucket_name);
+            storage_aws_role_arn = '|| iam_role ||'
+            storage_allowed_locations = ('|| bucket_name ||')';
+        EXECUTE IMMEDIATE :QUERY;
         RETURN message_out;
     END;
     $$;
