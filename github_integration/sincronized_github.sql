225e243e6a4ba9e00a5166eaa23357aad3f77bea
Ivan Felipe Caro Lemus <ivan.caro@globant.com>
2024-06-13 15:09:44 -0500

    Github folder

--- github_integration/sincronized_github.sql
+++ github_integration/sincronized_github.sql
@@ -15,19 +15,3 @@ create or replace git repository tutorial
     api_integration = git_api_integration
     git_credentials = github_secret
     origin = '< URL REPO >';
-
-
--- Show repos added to snowflake.
-show git repositories;
-
--- Show branches in the repo.
-show git branches in git repository tutorial;
-
--- List files.
-ls @tutorial/branches/main;
-
--- Show code in file.
-select $1 from @tutorial/branches/main/README.md;
-
--- Fetch git repository updates.
-alter git repository tutorial fetch;
